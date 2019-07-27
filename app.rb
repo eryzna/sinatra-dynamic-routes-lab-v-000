3b7bcbd499cff3fe3c332fa4a85dbab742c139e5
Elana Ryznar <eryzna@gmail.com>
2019-07-27 23:58:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    params[:number]
+
+  end
   # Write your code here!
 
 end
