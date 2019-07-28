a339145706103e020124e5db35b358314b1b791d
Elana Ryznar <eryzna@gmail.com>
2019-07-28 00:33:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,4 +19,8 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    (params[:number1].to_i params[:operation] params[:number2]).to_s
+  end
+
 end
