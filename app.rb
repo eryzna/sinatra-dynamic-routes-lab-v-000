38a8410f1e7db944eb655e9c1374c6eb89b7b9da
Elana Ryznar <eryzna@gmail.com>
2019-07-28 00:43:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,15 +31,7 @@ class App < Sinatra::Base
       (number1 * number2).to_s
     elsif params[:operation] == 'divide'
       (number1 / number2).to_s
-    else
-      puts 'Unable to perform this operation'
     end
-    #elsif params[:operation] == 'multiply'
-    #  (number1 * number2).to_s
-    #elsif params[:operation] == 'divide'
-    #  (number1 / number2).to_s
-    #else= 'Unable to perform this operation'
-    #end
   end
 
 end
